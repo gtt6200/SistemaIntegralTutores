3cab77fdec218555812ad3a63c0ebb0604d898b4
gtt6200 <gtt6200@gmail.com>
2023-01-25 22:04:03 -0600

    sidebar

--- SistemaTutores/src/app/app.module.ts
+++ SistemaTutores/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//Librería para hacer uso de los formularios de angular
+import { FormsModule } from "@angular/forms";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+//importamos el archivo app.routing.ts
+import { routing } from "./app.routing";
+
+/// Este archivo contiene la configuración inicial de todos los componentes y sus rutas
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //importamos las librerías añadidas
+    FormsModule,
+    HttpClientModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
